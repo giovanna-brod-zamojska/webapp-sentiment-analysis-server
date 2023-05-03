9709eaf0656ec88df6bd76b7354907815beca02b
Giovanna <gbrod1000@gmail.com>
2023-05-03 15:53:31 +0200

    setmongocontainerport

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 
 // Connect to MongoDB database
-mongoose.connect('mongodb://localhost:27017/my-database', {
+mongoose.connect('mongodb://172.17.0.1:27017/my-database', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
